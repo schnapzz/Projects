cf8ef40142a27938e0084738d4c56beb1d7ba965
Mikkel Muller-Hansen <mikmhansen@yahoo.dk>
2015-04-10 00:23:30 +0200

    Auto-layout in storyboard now works and a dummy of the sharedController aswell. Missing UITextField delegates to open/close keyboard on the fields.

--- Dept/Dept/CreatePersonViewController.m
+++ Dept/Dept/CreatePersonViewController.m
@@ -24,10 +24,17 @@
 
 - (void)createAction {
     
-    NSString* name = self.nameField.text;
-    double rented = [self.rentField.text doubleValue];
-    
-    [[Controller sharedController] createPersonWith:name and:rented];
+    if ([self isValidText:self.nameField.text] && [self isValidNumber:self.rentField.text]) {
+        
+        NSString* name = self.nameField.text;
+        double rented = [self.rentField.text doubleValue];
+        
+        [[Controller sharedController] createPersonWith:name and:rented];
+        
+    } else {
+        
+        NSLog(@"Couldn't create a person.");
+    }
 }
 
 /* Private methods */
@@ -36,16 +43,27 @@
     
     if ([text isNonEmpty]) {
         
-        
+        return YES;
     }
-    
-    
-    
+    return NO;
 }
 
 - (BOOL)isValidNumber:(NSString*)text {
     
+    NSString* pattern = @"^(?:|0|[1-9]\\d*)(?:\\.\\d*)?$";
+    NSRegularExpressionOptions regexOption = NSRegularExpressionCaseInsensitive;
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:pattern options:regexOption error:nil];
+
+    int matchingNumbers = [regex numberOfMatchesInString:text options:0 range:NSMakeRange(0, text.length)];
     
+//    NSCharacterSet *alphaNums = [NSCharacterSet decimalDigitCharacterSet];
+//    NSCharacterSet *inStringSet = [NSCharacterSet characterSetWithCharactersInString:myInputField.text];
+
+    if ([text isNonEmpty] && matchingNumbers == text.length) {
+        
+        return YES;
+    }
+    return NO;
 }
 
 @end
